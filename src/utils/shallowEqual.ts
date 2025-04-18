d0a0e47175439d271f677dec91b01d5ce09669ad
wadahkode <mvp.dedefilaras@gmail.com>
2025-04-18 05:33:42 +0000

    fix null

--- src/utils/shallowEqual.ts
+++ src/utils/shallowEqual.ts
@@ -1,14 +1,18 @@
 type Equal = Record<string, any>
 
-export function shallowEqual<T extends Equal>(
-  prev: T | undefined | null = {} as T,
-  current: T | undefined | null = {} as T
-): boolean {
+export const shallowEqual = <T extends Record<string, any> | null>(
+  prev: T,
+  current: T
+): boolean => {
+  if (prev === current) return true
+  if (!prev || !current) return false
+
   const prevKeys = Object.keys(prev)
   const currentKeys = Object.keys(current)
+
   if (prevKeys.length !== currentKeys.length) return false
 
-  for (const key of prevKeys) {
+  for (let key of prevKeys) {
     if (prev[key] !== current[key]) return false
   }
 
